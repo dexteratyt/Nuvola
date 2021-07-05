3dd255dbacaa4d863cd8ca481cef29e073ead265
DisabledMallis <mallisiscool@gmail.com>
2021-07-05 16:06:29 -0400

    More injector bs

--- Injector/Native/main.cpp
+++ Injector/Native/main.cpp
@@ -1,5 +1,7 @@
 #include <Windows.h>
+#include <tlhelp32.h>
 #include <string>
+#include <codecvt>
 #include <iostream>
 
 using namespace std;
@@ -10,9 +12,52 @@ enum class ArgState {
 	UNINJECT
 };
 
-auto main(int argc, char** argv) -> int {
-	cout << "Hello, World!" << endl;
+auto getProcId(const char* ProcName) -> int {
+	PROCESSENTRY32 pe32;
+	HANDLE hSnapshot = nullptr;
 
+	pe32.dwSize = sizeof(PROCESSENTRY32);
+	hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+	if(Process32First(hSnapshot, &pe32)) {
+		do {
+			//Goo goo ga ga wstring to string bad waaa
+			//Bitch idgaf suck my balls
+			using convert_type = std::codecvt_utf8<wchar_t>;
+			std::wstring_convert<convert_type, wchar_t> converter;
+			wstring wideName((WCHAR*)pe32.szExeFile);
+			string thinName = converter.to_bytes(wideName);
+			if(strcmp(thinName.c_str(), ProcName) == 0) {
+				break;
+			}
+		}
+		while(Process32Next(hSnapshot, &pe32));
+	}
+
+	if( hSnapshot != INVALID_HANDLE_VALUE )
+		CloseHandle( hSnapshot );
+
+	return pe32.th32ProcessID;
+}
+
+auto procIdFromName(string procName) -> int {
+	return getProcId(procName.c_str());
+}
+
+auto openProc(int procId) -> void* {
+	HANDLE procHandle = OpenProcess(0x1F0FFF, true, procId);
+	return procHandle;
+}
+auto openProc(string procName) -> void* {
+	return openProc(procIdFromName(procName));
+}
+
+
+auto injectDll() -> int {
+	return 0;
+}
+
+auto main(int argc, char** argv) -> int {
 	auto currentState = ArgState::NONE;
 	for(int i = 0; i < argc; i++) {
 		string argStr = string(argv[i]);
@@ -21,13 +66,23 @@ auto main(int argc, char** argv) -> int {
 			if(argStr == "-i") {
 				currentState = ArgState::INJECT;
 			}
+			if(argStr == "-u") {
+				currentState = ArgState::UNINJECT;
+			}
 		}
 		//If there is a Inject state, we are expecting a file name to inject.
 		if(currentState == ArgState::INJECT) {
 			string path = argStr;
 			//TODO: Inject dll
+			cout << "Injecting dll at " << path << endl;
 		}
-		cout << argv[i] << endl;
+		//If there is an Uninject state, we want the module to uninject
+		if(currentState == ArgState::UNINJECT) {
+			string moduleName = argStr;
+			//TODO: Uninject dll
+			cout << "Uninjecting module named " << moduleName << endl;
+		}
+		currentState = ArgState::NONE;
 	}
 	return 0;
 };
--- Lunity.bat
+++ Lunity.bat
@@ -1,40 +1,57 @@
+:: Awesome build script made by ASM#1010 :))
+
+:: Turn echo off
 @echo off
 echo Welcome to the epic gaming Lunity build script
+:: Main selection section
 :main
 echo Please select a choice below
 echo 1) Build Lunity (x64 only)
 echo 2) Inject
 echo 3) Clean project
+:: Reset the choice var
 set choice=
+:: Get input for choice var
 set /p choice=Make a selection:
 if not '%choice%'=='' set choice=%choice:~0,1%
+:: Jump to respective code
 if '%choice%' == '1' goto build
 if '%choice%' == '2' goto inject
 if '%choice%' == '3' goto clean
+:: Invalid choice, send them back to try again
 echo "%choice%" is not valid, try again...
 goto main
+
+:: Build section, actually build the project and output the generated files
 :build
+:: Update submodules
 git submodule update --init --recursive
 cd Lunity
 cd Lib
 cd PolyHook_2_0
+:: Build polyhook
 cmake -B"./_build" -DCMAKE_INSTALL_PREFIX="./_install/" -DPOLYHOOK_BUILD_SHARED_LIB=OFF
 cmake --build "./_build" --config Release --target INSTALL
 cd ..
 cd ..
 cd ..
+:: Build all of Lunity (Injector, Client, Etc)
 cmake .
 cmake --build . --config Release
+:: Put everything in an output folder
+mkdir Output
+copy /y Injector\Native\Release\Injector.exe Output\Injector.exe
+copy /y Lunity\Release\Lunity.dll Output\Lunity.dll
 goto end
+
+:: Call the injector program
 :inject
-cd Injector
-cd Native
-cd Release
+cd Output
 Injector.exe -i Lunity.dll
 cd ..
-cd ..
-cd ..
 goto end
+
+:: Clean-up files generated with CMake
 :clean
 rmdir /s /q "CMakeFiles"
 rmdir /s /q "x64"
@@ -48,6 +65,8 @@ rmdir /s /q "Lunity\Lunity.dir"
 rmdir /s /q "Lunity\Release"
 rmdir /s /q "Injector\Native\Injector.dir"
 rmdir /s /q "Injector\Native\Release"
+rmdir /s /q "Output"
+rmdir /s /q "build"
 goto end
 :end
 echo Task completed! (or failed, I don't know)
