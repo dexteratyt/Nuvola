51df787a827666b5b1499055380662db927236d7
CXCubeHD <cxcubehd@gmail.com>
2021-07-10 16:56:02 +0200

    Added D3D12Renderer and Event class (like in C#)

---
+++ Lunity/Client/Renderer/D3D12/D3D12Renderer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lukax on 10.07.2021.
+//
+
+#include "D3D12Renderer.h"
---
+++ Lunity/Client/Renderer/D3D12/D3D12Renderer.h
@@ -0,0 +1,15 @@
+#ifndef LUNITY_CLIENT_RENDERER_D3D12_D3D12RENDERER_H
+#define LUNITY_CLIENT_RENDERER_D3D12_D3D12RENDERER_H
+
+
+class D3D12Renderer
+{
+private:
+    bool isInitialized = false;
+public:
+    D3D12Renderer() { }
+    ~D3D12Renderer() { }
+};
+
+
+#endif //LUNITY_CLIENT_RENDERER_D3D12_D3D12RENDERER_H
