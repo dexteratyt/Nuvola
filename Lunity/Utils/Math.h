1b4ab8549c7b0af37b29428c03d0f43afca940c3
CXCubeHD <cxcubehd@gmail.com>
2021-07-14 21:58:09 +0200

    fixed errors

--- Lunity/Utils/Math.h
+++ Lunity/Utils/Math.h
@@ -9,23 +9,23 @@ struct Vector1
     T X = 0;
     Vector1() = default;
     Vector1(T x) : X(x) { }
-    Vector1 operator+(Vector1& other) const { return Vector1(X + other.X); }
-    Vector1 operator-(Vector1& other) const { return Vector1(X - other.X); }
-    Vector1 operator*(Vector1& other) const { return Vector1(X * other.X); }
-    Vector1 operator/(Vector1& other) const { return Vector1(X / other.X); }
-    Vector1& operator+=(Vector1& other) { X += other.X; return *this; }
-    Vector1& operator-=(Vector1& other) { X -= other.X; return *this; }
-    Vector1& operator*=(Vector1& other) { X *= other.X; return *this; }
-    Vector1& operator/=(Vector1& other) { X /= other.X; return *this; }
-    Vector1 operator+(T other) const { return Vector1(X + other); }
-    Vector1 operator-(T other) const { return Vector1(X - other); }
-    Vector1 operator*(T other) const { return Vector1(X * other); }
-    Vector1 operator/(T other) const { return Vector1(X / other); }
-    Vector1& operator+=(T other) { X += other; return *this; }
-    Vector1& operator-=(T other) { X -= other; return *this; }
-    Vector1& operator*=(T other) { X *= other; return *this; }
-    Vector1& operator/=(T other) { X /= other; return *this; }
-    T& operator[](int index) { *(this+(index*sizeof(T))); }
+    Vector1<T> operator+(Vector1<T>& other) const { return Vector1<T>(this->X + other.X); }
+    Vector1<T> operator-(Vector1<T>& other) const { return Vector1<T>(this->X - other.X); }
+    Vector1<T> operator*(Vector1<T>& other) const { return Vector1<T>(this->X * other.X); }
+    Vector1<T> operator/(Vector1<T>& other) const { return Vector1<T>(this->X / other.X); }
+    Vector1<T>& operator+=(Vector1<T>& other) { this->X += other.X; return *this; }
+    Vector1<T>& operator-=(Vector1<T>& other) { this->X -= other.X; return *this; }
+    Vector1<T>& operator*=(Vector1<T>& other) { this->X *= other.X; return *this; }
+    Vector1<T>& operator/=(Vector1<T>& other) { this->X /= other.X; return *this; }
+    Vector1<T> operator+(T other) const { return Vector1<T>(this->X + other); }
+    Vector1<T> operator-(T other) const { return Vector1<T>(this->X - other); }
+    Vector1<T> operator*(T other) const { return Vector1<T>(this->X * other); }
+    Vector1<T> operator/(T other) const { return Vector1<T>(this->X / other); }
+    Vector1<T>& operator+=(T other) { this->X += other; return *this; }
+    Vector1<T>& operator-=(T other) { this->X -= other; return *this; }
+    Vector1<T>& operator*=(T other) { this->X *= other; return *this; }
+    Vector1<T>& operator/=(T other) { this->X /= other; return *this; }
+    T& operator[](int index) { *(this + (index * sizeof(T))); }
 };
 
 template<typename T>
@@ -33,24 +33,24 @@ struct Vector2 : public Vector1<T>
 {
     T Y = 0;
     Vector2() = default;
-    Vector2(T x, T y) : Vector1(x), Y(y) { }
-    Vector2 operator+(Vector2& other) const { return Vector2(X + other.X, Y + other.Y); }
-    Vector2 operator-(Vector2& other) const { return Vector2(X - other.X, Y - other.Y); }
-    Vector2 operator*(Vector2& other) const { return Vector2(X * other.X, Y * other.Y); }
-    Vector2 operator/(Vector2& other) const { return Vector2(X / other.X, Y / other.Y); }
-    Vector2& operator+=(Vector2& other) { X += other.X; Y += other.Y; return *this; }
-    Vector2& operator-=(Vector2& other) { X -= other.X; Y -= other.Y; return *this; }
-    Vector2& operator*=(Vector2& other) { X *= other.X; Y *= other.Y; return *this; }
-    Vector2& operator/=(Vector2& other) { X /= other.X; Y /= other.Y; return *this; }
-    Vector2 operator+(T other) const { return Vector2(X + other, Y + other); }
-    Vector2 operator-(T other) const { return Vector2(X - other, Y - other); }
-    Vector2 operator*(T other) const { return Vector2(X * other, Y * other); }
-    Vector2 operator/(T other) const { return Vector2(X / other, Y / other); }
-    Vector2& operator+=(T other) { X += other; Y += other; return *this; }
-    Vector2& operator-=(T other) { X -= other; Y -= other; return *this; }
-    Vector2& operator*=(T other) { X *= other; Y *= other; return *this; }
-    Vector2& operator/=(T other) { X /= other; Y /= other; return *this; }
-    T& operator[](int index) { *(this+(index*sizeof(T))); }
+    Vector2(T x, T y) : Vector1<T>(x), Y(y) { }
+    Vector2<T> operator+(Vector2<T>& other) const { return Vector2<T>(this->X + other.X, this->Y + other.Y); }
+    Vector2<T> operator-(Vector2<T>& other) const { return Vector2<T>(this->X - other.X, this->Y - other.Y); }
+    Vector2<T> operator*(Vector2<T>& other) const { return Vector2<T>(this->X * other.X, this->Y * other.Y); }
+    Vector2<T> operator/(Vector2<T>& other) const { return Vector2<T>(this->X / other.X, this->Y / other.Y); }
+    Vector2<T>& operator+=(Vector2<T>& other) { this->X += other.X; this->Y += other.Y; return *this; }
+    Vector2<T>& operator-=(Vector2<T>& other) { this->X -= other.X; this->Y -= other.Y; return *this; }
+    Vector2<T>& operator*=(Vector2<T>& other) { this->X *= other.X; this->Y *= other.Y; return *this; }
+    Vector2<T>& operator/=(Vector2<T>& other) { this->X /= other.X; this->Y /= other.Y; return *this; }
+    Vector2<T> operator+(T other) const { return Vector2<T>(this->X + other, this->Y + other); }
+    Vector2<T> operator-(T other) const { return Vector2<T>(this->X - other, this->Y - other); }
+    Vector2<T> operator*(T other) const { return Vector2<T>(this->X * other, this->Y * other); }
+    Vector2<T> operator/(T other) const { return Vector2<T>(this->X / other, this->Y / other); }
+    Vector2<T>& operator+=(T other) { this->X += other; this->Y += other; return *this; }
+    Vector2<T>& operator-=(T other) { this->X -= other; this->Y -= other; return *this; }
+    Vector2<T>& operator*=(T other) { this->X *= other; this->Y *= other; return *this; }
+    Vector2<T>& operator/=(T other) { this->X /= other; this->Y /= other; return *this; }
+    T& operator[](int index) { *(this + (index * sizeof(T))); }
 };
 
 template<typename T>
@@ -58,24 +58,24 @@ struct Vector3 : public Vector2<T>
 {
     T Z = 0;
     Vector3() = default;
-    Vector3(T x, T y, T z) : Vector2(x, y), Z(z) { }
-    Vector3 operator+(Vector3& other) const { return Vector3(X + other.X, Y + other.Y, Z + other.Z); }
-    Vector3 operator-(Vector3& other) const { return Vector3(X - other.X, Y - other.Y, Z - other.Z); }
-    Vector3 operator*(Vector3& other) const { return Vector3(X * other.X, Y * other.Y, Z * other.Z); }
-    Vector3 operator/(Vector3& other) const { return Vector3(X / other.X, Y / other.Y, Z / other.Z); }
-    Vector3& operator+=(Vector3& other) { X += other.X; Y += other.Y; Z += other.Z; return *this; }
-    Vector3& operator-=(Vector3& other) { X -= other.X; Y -= other.Y; Z -= other.Z; return *this; }
-    Vector3& operator*=(Vector3& other) { X *= other.X; Y *= other.Y; Z *= other.Z; return *this; }
-    Vector3& operator/=(Vector3& other) { X /= other.X; Y /= other.Y; Z /= other.Z; return *this; }
-    Vector3 operator+(T other) const { return Vector3(X + other, Y + other, Z + other); }
-    Vector3 operator-(T other) const { return Vector3(X - other, Y - other, Z - other); }
-    Vector3 operator*(T other) const { return Vector3(X * other, Y * other, Z * other); }
-    Vector3 operator/(T other) const { return Vector3(X / other, Y / other, Z / other); }
-    Vector3& operator+=(T other) { X += other; Y += other; Z += other; return *this; }
-    Vector3& operator-=(T other) { X -= other; Y -= other; Z -= other; return *this; }
-    Vector3& operator*=(T other) { X *= other; Y *= other; Z *= other; return *this; }
-    Vector3& operator/=(T other) { X /= other; Y /= other; Z /= other; return *this; }
-    T& operator[](int index) { *(this+(index*sizeof(T))); }
+    Vector3(T x, T y, T z) : Vector2<T>(x, y), Z(z) { }
+    Vector3<T> operator+(Vector3<T>& other) const { return Vector3<T>(this->X + other.X, this->Y + other.Y, this->Z + other.Z); }
+    Vector3<T> operator-(Vector3<T>& other) const { return Vector3<T>(this->X - other.X, this->Y - other.Y, this->Z - other.Z); }
+    Vector3<T> operator*(Vector3<T>& other) const { return Vector3<T>(this->X * other.X, this->Y * other.Y, this->Z * other.Z); }
+    Vector3<T> operator/(Vector3<T>& other) const { return Vector3<T>(this->X / other.X, this->Y / other.Y, this->Z / other.Z); }
+    Vector3<T>& operator+=(Vector3<T>& other) { this->X += other.X; this->Y += other.Y; this->Z += other.Z; return *this; }
+    Vector3<T>& operator-=(Vector3<T>& other) { this->X -= other.X; this->Y -= other.Y; this->Z -= other.Z; return *this; }
+    Vector3<T>& operator*=(Vector3<T>& other) { this->X *= other.X; this->Y *= other.Y; this->Z *= other.Z; return *this; }
+    Vector3<T>& operator/=(Vector3<T>& other) { this->X /= other.X; this->Y /= other.Y; this->Z /= other.Z; return *this; }
+    Vector3<T> operator+(T other) const { return Vector3<T>(this->X + other, this->Y + other, this->Z + other); }
+    Vector3<T> operator-(T other) const { return Vector3<T>(this->X - other, this->Y - other, this->Z - other); }
+    Vector3<T> operator*(T other) const { return Vector3<T>(this->X * other, this->Y * other, this->Z * other); }
+    Vector3<T> operator/(T other) const { return Vector3<T>(this->X / other, this->Y / other, this->Z / other); }
+    Vector3<T>& operator+=(T other) { this->X += other; this->Y += other; this->Z += other; return *this; }
+    Vector3<T>& operator-=(T other) { this->X -= other; this->Y -= other; this->Z -= other; return *this; }
+    Vector3<T>& operator*=(T other) { this->X *= other; this->Y *= other; this->Z *= other; return *this; }
+    Vector3<T>& operator/=(T other) { this->X /= other; this->Y /= other; this->Z /= other; return *this; }
+    T& operator[](int index) { *(this + (index * sizeof(T))); }
 };
 
 template<typename T>
@@ -83,55 +83,55 @@ struct Vector4 : public Vector3<T>
 {
     T W = 0;
     Vector4() = default;
-    Vector4(T x, T y, T z, T w) : Vector3(x, y, z), W(w) { }
-    Vector4 operator+(Vector4& other) const { return Vector4(X + other.X, Y + other.Y, Z + other.Z, W + other.W); }
-    Vector4 operator-(Vector4& other) const { return Vector4(X - other.X, Y - other.Y, Z - other.Z, W - other.W); }
-    Vector4 operator*(Vector4& other) const { return Vector4(X * other.X, Y * other.Y, Z * other.Z, W * other.W); }
-    Vector4 operator/(Vector4& other) const { return Vector4(X / other.X, Y / other.Y, Z / other.Z, W / other.W); }
-    Vector4& operator+=(Vector4& other) { X += other.X; Y += other.Y; Z += other.Z; W += other.W; return *this; }
-    Vector4& operator-=(Vector4& other) { X -= other.X; Y -= other.Y; Z -= other.Z; W -= other.W; return *this; }
-    Vector4& operator*=(Vector4& other) { X *= other.X; Y *= other.Y; Z *= other.Z; W *= other.W; return *this; }
-    Vector4& operator/=(Vector4& other) { X /= other.X; Y /= other.Y; Z /= other.Z; W /= other.W; return *this; }
-    Vector4 operator+(T other) const { return Vector4(X + other, Y + other, Z + other, W + other); }
-    Vector4 operator-(T other) const { return Vector4(X - other, Y - other, Z - other, W - other); }
-    Vector4 operator*(T other) const { return Vector4(X * other, Y * other, Z * other, W * other); }
-    Vector4 operator/(T other) const { return Vector4(X / other, Y / other, Z / other, W / other); }
-    Vector4& operator+=(T other) { X += other; Y += other; Z += other; W += other; return *this; }
-    Vector4& operator-=(T other) { X -= other; Y -= other; Z -= other; W -= other; return *this; }
-    Vector4& operator*=(T other) { X *= other; Y *= other; Z *= other; W *= other; return *this; }
-    Vector4& operator/=(T other) { X /= other; Y /= other; Z /= other; W /= other; return *this; }
-    T& operator[](int index) { *(this+(index*sizeof(T))); }
+    Vector4(T x, T y, T z, T w) : Vector3<T>(x, y, z), W(w) { }
+    Vector4<T> operator+(Vector4<T>& other) const { return Vector4(this->X + other.X, this->Y + other.Y, this->Z + other.Z, this->W + other.W); }
+    Vector4<T> operator-(Vector4<T>& other) const { return Vector4(this->X - other.X, this->Y - other.Y, this->Z - other.Z, this->W - other.W); }
+    Vector4<T> operator*(Vector4<T>& other) const { return Vector4(this->X * other.X, this->Y * other.Y, this->Z * other.Z, this->W * other.W); }
+    Vector4<T> operator/(Vector4<T>& other) const { return Vector4(this->X / other.X, this->Y / other.Y, this->Z / other.Z, this->W / other.W); }
+    Vector4<T>& operator+=(Vector4<T>& other) { this->X += other.X; this->Y += other.Y; this->Z += other.Z; this->W += other.W; return *this; }
+    Vector4<T>& operator-=(Vector4<T>& other) { this->X -= other.X; this->Y -= other.Y; this->Z -= other.Z; this->W -= other.W; return *this; }
+    Vector4<T>& operator*=(Vector4<T>& other) { this->X *= other.X; this->Y *= other.Y; this->Z *= other.Z; this->W *= other.W; return *this; }
+    Vector4<T>& operator/=(Vector4<T>& other) { this->X /= other.X; this->Y /= other.Y; this->Z /= other.Z; this->W /= other.W; return *this; }
+    Vector4<T> operator+(T other) const { return Vector4<T>(this->X + other, this->Y + other, this->Z + other, this->W + other); }
+    Vector4<T> operator-(T other) const { return Vector4<T>(this->X - other, this->Y - other, this->Z - other, this->W - other); }
+    Vector4<T> operator*(T other) const { return Vector4<T>(this->X * other, this->Y * other, this->Z * other, this->W * other); }
+    Vector4<T> operator/(T other) const { return Vector4<T>(this->X / other, this->Y / other, this->Z / other, this->W / other); }
+    Vector4<T>& operator+=(T other) { this->X += other; this->Y += other; this->Z += other; this->W += other; return *this; }
+    Vector4<T>& operator-=(T other) { this->X -= other; this->Y -= other; this->Z -= other; this->W -= other; return *this; }
+    Vector4<T>& operator*=(T other) { this->X *= other; this->Y *= other; this->Z *= other; this->W *= other; return *this; }
+    Vector4<T>& operator/=(T other) { this->X /= other; this->Y /= other; this->Z /= other; this->W /= other; return *this; }
+    T& operator[](int index) { *(this + (index * sizeof(T))); }
 };
 
-struct Vector1F : public Vector1<float> { };
-struct Vector2F : public Vector2<float> { };
-struct Vector3F : public Vector3<float> { };
-struct Vector4F : public Vector4<float> { };
+struct Vector1F : public Vector1<float> { using Vector1<float>::Vector1; };
+struct Vector2F : public Vector2<float> { using Vector2<float>::Vector2; };
+struct Vector3F : public Vector3<float> { using Vector3<float>::Vector3; };
+struct Vector4F : public Vector4<float> { using Vector4<float>::Vector4; };
 
-struct Vector1D : public Vector1<double> { };
-struct Vector2D : public Vector2<double> { };
-struct Vector3D : public Vector3<double> { };
-struct Vector4D : public Vector4<double> { };
+struct Vector1D : public Vector1<double> { using Vector1<double>::Vector1; };
+struct Vector2D : public Vector2<double> { using Vector2<double>::Vector2; };
+struct Vector3D : public Vector3<double> { using Vector3<double>::Vector3; };
+struct Vector4D : public Vector4<double> { using Vector4<double>::Vector4; };
 
-struct Vector1B : public Vector1<char> { };
-struct Vector2B : public Vector2<char> { };
-struct Vector3B : public Vector3<char> { };
-struct Vector4B : public Vector4<char> { };
+struct Vector1B : public Vector1<char> { using Vector1<char>::Vector1; };
+struct Vector2B : public Vector2<char> { using Vector2<char>::Vector2; };
+struct Vector3B : public Vector3<char> { using Vector3<char>::Vector3; };
+struct Vector4B : public Vector4<char> { using Vector4<char>::Vector4; };
 
-struct Vector1S : public Vector1<short> { };
-struct Vector2S : public Vector2<short> { };
-struct Vector3S : public Vector3<short> { };
-struct Vector4S : public Vector4<short> { };
+struct Vector1S : public Vector1<short> { using Vector1<short>::Vector1; };
+struct Vector2S : public Vector2<short> { using Vector2<short>::Vector2; };
+struct Vector3S : public Vector3<short> { using Vector3<short>::Vector3; };
+struct Vector4S : public Vector4<short> { using Vector4<short>::Vector4; };
 
-struct Vector1I : public Vector1<int> { };
-struct Vector2I : public Vector2<int> { };
-struct Vector3I : public Vector3<int> { };
-struct Vector4I : public Vector4<int> { };
+struct Vector1I : public Vector1<int> { using Vector1<int>::Vector1; };
+struct Vector2I : public Vector2<int> { using Vector2<int>::Vector2; };
+struct Vector3I : public Vector3<int> { using Vector3<int>::Vector3; };
+struct Vector4I : public Vector4<int> { using Vector4<int>::Vector4; };
 
-struct Vector1L : public Vector1<long long> { };
-struct Vector2L : public Vector2<long long> { };
-struct Vector3L : public Vector3<long long> { };
-struct Vector4L : public Vector4<long long> { };
+struct Vector1L : public Vector1<long long> { using Vector1<long long>::Vector1; };
+struct Vector2L : public Vector2<long long> { using Vector2<long long>::Vector2; };
+struct Vector3L : public Vector3<long long> { using Vector3<long long>::Vector3; };
+struct Vector4L : public Vector4<long long> { using Vector4<long long>::Vector4; };
 
 /*
 
