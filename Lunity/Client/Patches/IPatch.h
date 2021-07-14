84494ec27f5cdcdf3a4400b9e1c02aa753d97140
DisabledMallis <mallisiscool@gmail.com>
2021-07-14 17:04:10 -0400

    Update IPatch.h

--- Lunity/Client/Patches/IPatch.h
+++ Lunity/Client/Patches/IPatch.h
@@ -11,7 +11,10 @@
 
 class IPatch
 {
+	//The main disasembler
 	static inline PLH::CapstoneDisassembler* dis = new PLH::CapstoneDisassembler(PLH::Mode::x64);
+
+	//Patch metadata
 	std::string name;
 	std::vector<SigInfo*>* signatures;
 public:
@@ -22,8 +25,11 @@ public:
 	void AddSignature(SigInfo* sigInfo);
 	//Scans all sigs in the order
 	auto ScanSigs() -> uintptr_t;
+	//The patches name
 	auto GetName() -> std::string;
+	//Returns the polyhook disassembler. You might not need this.
 	auto GetDis() -> PLH::CapstoneDisassembler&;
+	//Actually apply the hook
 	virtual auto Apply() -> bool;
 };
 
