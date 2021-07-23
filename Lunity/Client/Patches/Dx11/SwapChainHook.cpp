91c8e72e83f824c1d5c341138ec1097286cff5e9
DisabledMallis <mallisiscool@gmail.com>
2021-07-23 04:54:38 -0400

    idk wtf im doing

--- Lunity/Client/Patches/Dx11/SwapChainHook.cpp
+++ Lunity/Client/Patches/Dx11/SwapChainHook.cpp
@@ -14,15 +14,15 @@ returnPoint:
 	return PLH::FnCast(presentOriginal, &hookPresent)(pChain, syncInterval, flags);
 }
 
-SwapChainHook::SwapChainHook()  : IPatch::IPatch("Dx11::Present") {
+SwapChainHook::SwapChainHook()  : IPatch::IPatch("Dx12::Present") {
 	
 }
 
 auto SwapChainHook::Apply() -> bool {
-	if (kiero::init(kiero::RenderType::D3D11) == kiero::Status::Success) {
-		Utils::DebugF("Kiero setup for d3d11");
+	if (kiero::init(kiero::RenderType::D3D12) == kiero::Status::Success) {
+		Utils::DebugF("Kiero setup for d3d12");
 
-		if(kiero::bind(8, (void**)&presentOriginal, hookPresent) == kiero::Status::Success) {
+		if(kiero::bind(140, (void**)&presentOriginal, hookPresent) == kiero::Status::Success) {
 			return true;
 		}
 	}
---
+++ Lunity/Utils/Renderer.cpp
@@ -0,0 +1,31 @@
+#include "Renderer.h"
+
+void Renderer::Begin() {
+
+}
+void Renderer::End() {
+
+}
+
+void Renderer::SetLineWidth(float width) {
+
+}
+void Renderer::SetColor(Vector3<float>) {
+
+}
+void Renderer::SetColor(float r, float g, float b) {
+
+}
+void Renderer::SetColor(Vector4<float>) {
+
+}
+void Renderer::SetColor(float r, float g, float b, float a) {
+
+}
+
+void Renderer::Vertex2D(float x, float y) {
+	
+}
+void Renderer::Vertex2D(Vector2<float>) {
+	
+}
