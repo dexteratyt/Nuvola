1338069c699de999abf6e84988248e320123d005
DisabledMallis <mallisiscool@gmail.com>
2021-09-08 23:55:47 -0400

    mmm comments

--- Lunity/Client/Features/Modules/Render/ModList.cpp
+++ Lunity/Client/Features/Modules/Render/ModList.cpp
@@ -11,13 +11,18 @@ ModList::ModList() : Module("ModList") {
 }
 
 void ModList::onRenderEvent(RenderEvent& event) {
+	//Get the wrapper & vars we need
 	MinecraftRenderer* wrapper = event.GetRenderWrapper();
 	GuiData* data = event.GetRenderContext()->clientInstance->guiData;
 	Vector2<float> scaledRes = data->scaledResolution;
 	
+	//Loop all mods
 	int i = 0;
 	for(auto mod : *ModuleMgr::getInstance()->getAllModules()) {
+		//Check if theyre enabled
 		if(mod->IsEnabled()) {
+			//Render it if its enabled
+			//TODO: Super fancy animations
 			std::string name = mod->getName();
 			float width = wrapper->MeasureText(name);
 
